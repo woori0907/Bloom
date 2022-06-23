737d353e1fd0120317b57501dff6c17259eac559
Woori <rose5307@naver.com>
2022-06-23 22:09:15 +0900

    front-end setup

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ import express from 'express';
 
 const app = express();
 
-console.log('hello');
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(__dirname + "/public"));
+app.get('/', (req,res) => res.render('home'));
+
+console.log('helloooooooooo');
 
 app.listen(3000);
